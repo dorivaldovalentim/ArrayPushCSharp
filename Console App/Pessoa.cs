56e7206b45f5de6ef8cf02ed14ece39958719544
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2021-11-10 10:15:59 +0100

    Adding the project files

---
+++ Console App/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_App
+{
+
+   class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Idade { get; set; }
+        public string Sexo { get; set; }
+        public string Nacionalidade { get; set; }
+        public string Altura { get; set; }
+
+        public Pessoa(string nome, string idade, string sexo, string nacionalidade, string altura)
+        {
+            Nome = nome;
+            Idade = idade;
+            Sexo = sexo;
+            Nacionalidade = nacionalidade;
+            Altura = altura;
+        }
+    }
+}
---
+++ Console App/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using static Console_App.Pessoa;
+
+namespace Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 1;
+            ArrayList Users = new ArrayList();
+
+            Console.WriteLine("Hello World!");
+
+            for(i = 1; i <= 2; i++)
+            {
+                Console.WriteLine("Insira seu nome");
+                var nome = Console.ReadLine();
+
+                Console.WriteLine("Insira a sua idade");
+                var idade = Console.ReadLine();
+
+                Console.WriteLine("Insira o seu sexo");
+                var sexo = Console.ReadLine();
+                
+                Console.WriteLine("Insira a sua nacionalidade");
+                var nacionalidade = Console.ReadLine();
+
+                Console.WriteLine("Insira a sua altura");
+                var altura = Console.ReadLine();
+
+                Pessoa pessoa = new Pessoa(nome, idade, sexo, nacionalidade, altura);
+                Users.Add(pessoa);
+            }
+
+            Console.WriteLine("Final!");
+        }
+    }
+}
